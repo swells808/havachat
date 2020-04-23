793e974f649c565343d79ff42bb06f5d6b21f3ee
Sean Wells <seanw@commecebeacon.com>
2020-04-23 16:26:00 -0700

    Initial Commit

---
+++ havachat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  havachat
+//
+//  Created by Sean Wells on 4/23/20.
+//  Copyright © 2020 Sean Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
